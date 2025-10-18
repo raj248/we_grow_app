b15e3217ed16dad443a59d30d2c75e8da694fc06
raj248 <shashankraj007281@gmail.com>
2025-10-18 15:53:48 +0530

    version up

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: IS_DEV ? 'we_grow(dev)' : 'YouReach Booster',
     slug: 'you-reach-booster',
-    version: '1.0.5b',
+    version: '1.0.6b',
     scheme: 'you-reach-booster',
     web: {
       bundler: 'metro',
